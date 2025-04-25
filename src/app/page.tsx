5774690a8882a12d0f00782cdca59c539d396335
Paul Roder <mail@fled.dev>
2025-04-26 00:12:43 +0200

    Added nav bar, some minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Hero from "./components/Hero";
+import styles from "./styles/navigation.module.css";
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className={`min-h-screen font-[family-name:var(--font-geist-sans)] ${styles.contentPadding}`}>
       <Hero />
       
       <div className="p-8 sm:p-20">
