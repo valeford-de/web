71d7c3a20453e633f5b3257271374728fbc6d2cc
fled-dev <fled-dev@users.noreply.github.com>
2025-06-25 17:18:28 +0000

    deploy: d0eb3ac6b2bebd4602b52024c37321a66b3a7ffc

--- _next/static/chunks/app/layout-61587ff50f8cfd3b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4897:function(n,e,u){Promise.resolve().then(u.bind(u,6032))}},function(n){n.O(0,[220,640,385,971,938,744],function(){return n(n.s=4897)}),_N_E=n.O()}]);
